338be050572e801d245560dcfee710c4c5b55432
garebareDA <garedaiki@gmail.com>
2022-08-14 04:27:56 +0900

    hot fix

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,13 +4,13 @@ use std::collections::VecDeque;
 fn main() {
     let mut rng = rand::thread_rng();
 
-    let ddsk_vec = vec!["ドド", "スコ"];
+    let ddsk_slice: [&str; 2] = ["ドド", "スコ"];
     let match_ward = "ドドスコスコスコドドスコスコスコドドスコスコスコ";
     let mut choice_ddsks:VecDeque<&str> = VecDeque::from([""; 12]);
 
     loop {
         let random = rng.gen_range(0..2);
-        let choice_ddsk = ddsk_vec[random];
+        let choice_ddsk = ddsk_slice[random];
         choice_ddsks.pop_back();
         choice_ddsks.push_front(choice_ddsk);
         let ddsk = choice_ddsks.as_slices().0.join("");
