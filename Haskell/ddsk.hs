8d9abe17f668c66af9a2672ed7cfc93b00528a50
rytst <r.saito0111@gmail.com>
2024-08-07 07:07:55 +0900

    polish DDSK

--- Haskell/ddsk.hs
+++ Haskell/ddsk.hs
@@ -2,21 +2,16 @@ import System.Random
 import Control.Monad
 
 main = do
-    g    <- newStdGen       -- get a new random generator
+    g <- newStdGen
     let args = ["ドド", "スコ"]
-
     let ns   = randomRs (0,length args-1) g
         strs = map (args !!) ns
-
     let (_, res) = isDdsk initstr strs []
     mapM_ putStr res
     putStr "ラブ注入♡"
 
-
-
 initstr :: [String]
 initstr = ["X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X"]
-
 
 ddskstr :: [String]
 ddskstr = ["ドド", "スコ", "スコ", "スコ", "ドド", "スコ", "スコ", "スコ", "ドド", "スコ", "スコ", "スコ"]
