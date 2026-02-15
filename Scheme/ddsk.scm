31111b13ce3057f35114a31a9c2b5f6ac8651e43
T Renkon <12984930+Mamonbo@users.noreply.github.com>
2026-02-15 20:18:20 +0900

    remove debug things
    - depth limt
    - printing internal list

--- Scheme/ddsk.scm
+++ Scheme/ddsk.scm
@@ -8,31 +8,28 @@
 ;; shorter version for debugging
 ;;(define ddsk-goal "ドドスコスコスコ")
 
-(define (dd-loop txtlst depth)
+(define (dd-loop txtlst)
   (let* (
 	;; pick "ドド" or "スコ"
 	(next-elem (if (equal? (random-integer 2) 0) "ドド" "スコ"))
 	(next-txtlst (append txtlst (cons next-elem '())))
 	)
-;;    (print next-txtlst)
     (print next-elem)
     ;; ddsk check
     (when
-	(not (or (string-contains (string-join next-txtlst "") ddsk-goal)
-		 ;; note: skipping delimiter means delim=" " automatically
-	     (> depth 10000) )
-	     )
-      ;; order: contains text word
+	(not  (string-contains (string-join next-txtlst "") ddsk-goal)
+	      ;; note: skipping delimiter means delim=" " automatically
+	      ;; order: contains text word
+	      )
       (dd-loop (if (> (length next-txtlst) buffer-length-elements)
 		   (cdr next-txtlst)
-		   next-txtlst
-		   ) (+ 1 depth))
+		   next-txtlst))
       )
     )
   )
 
 ;; main() part
 (random-source-randomize! default-random-source); initialize
-(dd-loop '() 0)
+(dd-loop '())
 (print "ラブ注入❤")
 
